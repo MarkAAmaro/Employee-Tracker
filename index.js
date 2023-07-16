175c3b19ba36d0a0e0c9f624a8250a135b462cae
Mark Anthony <manthonyamaro@yahoo.com>
2023-07-16 18:58:10 -0500

    set first prompt information based on requirements

--- index.js
+++ index.js
@@ -1,10 +1,42 @@
 const mysql = require('mysql2/promise');
 const inquirer = require('inquirer');
+require('dotenv').config();
+
+const main = async () => {
+    const connection = await mysql.createConnection({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
+    });
 
 const main = async () => {
     const connection = await mysql.createConnection({
         host: 'localhost',
         user: 'root',
-        password: 'Ardrias1!',  // use your MySQL password here
+        password: 'Ardrias1!', 
         database: 'my_db'
     });
+
+    console.log('Connected to the database.');
+
+    const { action } = await inquirer.prompt([
+        {
+            type: 'list',
+            name: 'action',
+            message: 'What would you like to do?',
+            choices: [
+                'View all departments',
+                'View all roles',
+                'View all employees',
+                'Add a department',
+                'Add a role',
+                'Add an employee',
+                'Update an employee role',
+                'Exit'
+            ]
+        }
+    ]);}}
+
+    main()
+    .catch(err => console.error(err));
