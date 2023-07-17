4a8736f0676ad7dd7adb8d5b327e3b2ba09ce3a9
Mark Anthony <manthonyamaro@yahoo.com>
2023-07-17 18:32:10 -0500

    set each function for viewing dpeartments,employees and roles to TAKE CONNECTIONS AND SHOW THE QUERY USINg sql into a table and set the employee table to demostrate the join operation when adding a new employee with updated information from the options provided

--- index.js
+++ index.js
@@ -69,5 +69,38 @@ const main = async () => {
     
     main();
 }
+
+const viewAllDepartments = async (connection) => {
+    const [rows] = await connection.execute('SELECT * FROM department');
+    console.table(rows);
+}
+
+const viewAllRoles = async (connection) => {
+    const [rows] = await connection.execute('SELECT * FROM role');
+    console.table(rows);
+}
+
+const viewAllEmployees = async (connection) => {
+    try {
+        const query = `
+            SELECT 
+                e.id, 
+                e.first_name, 
+                e.last_name, 
+                r.title AS role,
+                d.name AS department,
+                CONCAT(m.first_name, ' ', m.last_name) AS manager
+            FROM employee e
+            LEFT JOIN role r ON e.role_id = r.id
+            LEFT JOIN department d ON r.department_id = d.id
+            LEFT JOIN employee m ON e.manager_id = m.id
+        `;
+        
+        const [rows] = await connection.execute(query);
+        console.table(rows);
+    } catch (error) {
+        console.error("Error in viewAllEmployees function:", error.message);
+    }
+}
     main()
     .catch(err => console.error(err));
